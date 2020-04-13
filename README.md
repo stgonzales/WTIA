09555bbebc0fe0f6f01913f661cc7959f38023ad
Stephen Goncalves <stiefo.o@gmail.com>
2020-04-13 19:11:02 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# WTIA
-A VueJS project that show on a map where the ISS at.
+# wtia
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Iss.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="hello">
+    <div v-if="loading">
+      <p>Loading ...</p>
+    </div>
+
+    <div v-if="err">
+      {{err}}
+    </div>
+
+    <div v-if="center" id="mapid">
+      <l-map style="height: 400px" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="center" :icon="icon"></l-marker>
+      </l-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { latLng } from "leaflet";
+import L from 'leaflet';
+
+export default {
+  name: 'Iss',
+  data(){
+    return{
+      loading: true,
+      err: null,
+      post: null,
+      zoom: 5,
+      center: null,
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      currentZoom: 11.5,
+      showParagraph: false,
+      icon:L.icon({
+        iconUrl: '../assets/logo.png',
+        iconSize: [32, 37],
+        iconAnchor: [16, 37]
+      }),
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
+    }
+  },
+  created(){
+    this.fetchData()
+  },
+  beforeDestroy(){
+
+  },
+  methods:{
+    fetchData(){
+      setInterval(() => {this.err = this.post = null
+        axios.get('https://api.wheretheiss.at/v1/satellites/25544')
+          .then(res => {
+            this.loading = false
+            this.center = latLng(res.data.latitude, res.data.longitude)
+          })
+          .catch(err => this.err = err)
+      },3000)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#mapid { 
+  width: 850px;
+  margin: auto;
+}
+</style>
